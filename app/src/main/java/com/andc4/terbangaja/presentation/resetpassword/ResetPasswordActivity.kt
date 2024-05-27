4241dd3a5afc587335a647834498c71091ee90fc
devotionfaith <rgusti530@gmail.com>
2024-05-27 20:36:06 +0700

    Creating Base Reset Page Interface

---
+++ app/src/main/java/com/andc4/terbangaja/presentation/resetpassword/ResetPasswordActivity.kt
@@ -0,0 +1,19 @@
+package com.andc4.terbangaja.presentation.resetpassword
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.andc4.terbangaja.R
+import com.andc4.terbangaja.databinding.ActivityResetPasswordBinding
+
+class ResetPasswordActivity : AppCompatActivity() {
+    private val binding : ActivityResetPasswordBinding by lazy {
+        ActivityResetPasswordBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
