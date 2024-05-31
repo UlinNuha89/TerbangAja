988fac247fd2acbef5d11a103a79f7f4cf7c5a2e
devotionfaith <rgusti530@gmail.com>
2024-05-31 20:03:45 +0700

    Creating Form Validation

--- app/src/main/java/com/andc4/terbangaja/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/andc4/terbangaja/presentation/login/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.andc4.terbangaja.presentation.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.andc4.terbangaja.R
 import com.andc4.terbangaja.databinding.ActivityLoginBinding
 import com.andc4.terbangaja.presentation.main.MainActivity
 import com.andc4.terbangaja.presentation.register.RegisterActivity
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
@@ -15,19 +20,75 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupForm()
         setClickListener()
     }
 
     private fun setClickListener() {
         binding.tvNavToRegister.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
+            navigateToRegister()
         }
         binding.btnLogin.setOnClickListener {
-            startActivity(
-                Intent(this, MainActivity::class.java).apply {
-                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                },
-            )
+            doLogin()
         }
+    }
+
+    private fun doLogin() {
+        if (isFormValid()) {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupForm() {
+        binding.tilPassword.isVisible = true
+        binding.tilEmail.isVisible = true
+    }
+
+    private fun isFormValid(): Boolean {
+        val email = binding.etEmail.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+
+        return validateEmail(email) && validatePassword(password, binding.tilPassword)
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        val errorMsg =
+            when {
+                email.isEmpty() -> getString(R.string.text_error_email_empty)
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> getString(R.string.text_error_email_invalid)
+                else -> null
+            }
+        binding.tilEmail.isErrorEnabled = errorMsg != null
+        binding.tilEmail.error = errorMsg
+        return errorMsg == null
+    }
+
+    private fun validatePassword(
+        password: String,
+        textInputLayout: TextInputLayout,
+    ): Boolean {
+        val errorMsg =
+            when {
+                password.isEmpty() -> getString(R.string.text_error_password_empty)
+                password.length < 8 -> getString(R.string.text_error_password_less_than_8_char)
+                else -> null
+            }
+        textInputLayout.isErrorEnabled = errorMsg != null
+        textInputLayout.error = errorMsg
+        return errorMsg == null
+    }
+
+    private fun navigateToMain() {
+        startActivity(
+            Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            },
+        )
+    }
+
+    private fun navigateToRegister() {
+        startActivity(Intent(this, RegisterActivity::class.java))
     }
 }
--- app/src/main/java/com/andc4/terbangaja/presentation/register/RegisterActivity.kt
+++ app/src/main/java/com/andc4/terbangaja/presentation/register/RegisterActivity.kt
@@ -2,10 +2,14 @@ package com.andc4.terbangaja.presentation.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.andc4.terbangaja.R
 import com.andc4.terbangaja.databinding.ActivityRegisterBinding
 import com.andc4.terbangaja.presentation.login.LoginActivity
-import com.andc4.terbangaja.presentation.otp.OtpActivity
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
     private val binding: ActivityRegisterBinding by lazy {
@@ -15,6 +19,15 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupForm()
+    }
+
+    private fun setupForm() {
+        binding.tilConfirmPassword.isVisible = true
+        binding.tilTelp.isVisible = true
+        binding.tilName.isVisible = true
+        binding.tilPassword.isVisible = true
+        binding.tilEmail.isVisible = true
         setClickListener()
     }
 
@@ -23,14 +36,148 @@ class RegisterActivity : AppCompatActivity() {
             onBackPressed()
         }
         binding.btnDaftar.setOnClickListener {
-            startActivity(Intent(this, OtpActivity::class.java))
+            doRegistration()
         }
         binding.tvNavToLogin.setOnClickListener {
-            startActivity(
-                Intent(this, LoginActivity::class.java).apply {
-                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                },
-            )
+            navigateToLogin()
         }
+    }
+
+    private fun doRegistration() {
+        if (isFormValid()) {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            val confPassword = binding.etConfirmPassword.text.toString().trim()
+            val name = binding.etName.text.toString().trim()
+            val telp = binding.etTelp.text.toString().trim()
+            Toast.makeText(this, "Regist", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isFormValid(): Boolean {
+        val email = binding.etEmail.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+        val confPassword = binding.etConfirmPassword.text.toString().trim()
+        val name = binding.etName.text.toString().trim()
+        val telp = binding.etTelp.text.toString().trim()
+
+        return validateAllFieldsFilled(email, password, confPassword, name, telp) &&
+            validateEmail(email) &&
+            validatePassword(password, binding.tilPassword) &&
+            validatePassword(confPassword, binding.tilConfirmPassword) &&
+            passwordsMatch(password, confPassword) &&
+            validatePhoneNumber(telp)
+    }
+
+    private fun validatePhoneNumber(telp: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    private fun validateAllFieldsFilled(
+        email: String,
+        password: String,
+        confPassword: String,
+        name: String,
+        telp: String,
+    ): Boolean {
+        var isValid = true
+
+        if (email.isEmpty()) {
+            binding.tilEmail.isErrorEnabled = true
+            binding.tilEmail.error = getString(R.string.text_error_email_empty)
+            isValid = false
+        } else {
+            binding.tilEmail.isErrorEnabled = false
+        }
+
+        if (password.isEmpty()) {
+            binding.tilPassword.isErrorEnabled = true
+            binding.tilPassword.error = getString(R.string.text_error_password_empty)
+            isValid = false
+        } else {
+            binding.tilPassword.isErrorEnabled = false
+        }
+
+        if (confPassword.isEmpty()) {
+            binding.tilConfirmPassword.isErrorEnabled = true
+            binding.tilConfirmPassword.error = getString(R.string.text_error_password_empty)
+            isValid = false
+        } else {
+            binding.tilConfirmPassword.isErrorEnabled = false
+        }
+
+        if (name.isEmpty()) {
+            binding.tilName.isErrorEnabled = true
+            binding.tilName.error = getString(R.string.text_error_name_empty)
+            isValid = false
+        } else {
+            binding.tilName.isErrorEnabled = false
+        }
+
+        if (telp.isEmpty()) {
+            binding.tilTelp.isErrorEnabled = true
+            binding.tilTelp.error = getString(R.string.text_error_telp_empty)
+            isValid = false
+        } else {
+            binding.tilTelp.isErrorEnabled = false
+        }
+        return isValid
+    }
+
+    private fun passwordsMatch(
+        password: String,
+        confirmPassword: String,
+    ): Boolean {
+        return if (password != confirmPassword) {
+            showPasswordMismatchError(true)
+            false
+        } else {
+            showPasswordMismatchError(false)
+            true
+        }
+    }
+
+    private fun showPasswordMismatchError(isError: Boolean) {
+        binding.tilConfirmPassword.isErrorEnabled = isError
+        if (isError) {
+            val errorMsg = getString(R.string.text_password_does_not_match)
+            binding.tilConfirmPassword.error = errorMsg
+        }
+    }
+
+    private fun validatePassword(
+        password: String,
+        textInputLayout: TextInputLayout,
+    ): Boolean {
+        val errorMsg =
+            when {
+                password.isEmpty() -> getString(R.string.text_error_password_empty)
+                password.length < 8 -> getString(R.string.text_error_password_less_than_8_char)
+                else -> null
+            }
+        textInputLayout.isErrorEnabled = errorMsg != null
+        textInputLayout.error = errorMsg
+        return errorMsg == null
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        val errorMsg =
+            when {
+                email.isEmpty() -> getString(R.string.text_error_email_empty)
+                !Patterns.EMAIL_ADDRESS.matcher(email)
+                    .matches() -> getString(R.string.text_error_email_invalid)
+                else -> null
+            }
+        binding.tilEmail.isErrorEnabled = errorMsg != null
+        binding.tilEmail.error = errorMsg
+        return errorMsg == null
+    }
+
+    private fun navigateToLogin() {
+        startActivity(
+            Intent(this, LoginActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            },
+        )
     }
 }
